4b73644caaab51e0fb594c42188ca4298fa27cac
cregithar <869725317@qq.com>
2021-03-01 18:14:00 +0800

    test

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -2,7 +2,7 @@ package helloword;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		System.out.println("hello world!  update-1");
 	}
 
 }
